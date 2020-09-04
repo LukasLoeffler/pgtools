d51a0beaf65671cc72117525fc65ff3311881d5d
Lukas Löffler <lukasloeffler96@googlemail.com>
2020-09-04 12:46:48 +0200

    Removed unused about page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
 import Triggers from '../views/Triggers.vue'
 import Watcher from '../views/Watcher.vue'
 import Connections from '../views/Connections.vue'
@@ -13,11 +12,6 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
   },
   {
     path: '/triggers',
--- client/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <v-chip color="green"></v-chip>
-  </div>
-</template>
