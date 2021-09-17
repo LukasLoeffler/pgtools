4a8314c7ed2b1ac8a562d9a299f7d30ffeb3a2d1
Lukas Löffler <lukas.loeffler@ibiz-innovation.de>
2021-09-17 18:48:06 +0200

    Refactored styling of command output

--- client/src/components/commands/CommandOutput.vue
+++ client/src/components/commands/CommandOutput.vue
@@ -1,22 +1,23 @@
 <template>
-  <v-card dark class="ml-1" ref="cardMain" style="height: calc(100vh - 75px)">
-
-    <v-card-title ref="cardTitle">
-      <v-row class="ml-1">
-            <v-col cols="6"> 
+  <v-card dark class="ml-1" v-resize="updateTableDistance">
+    <v-card-title>
+      <v-row>
+            <v-col cols="8"> 
               <span class="headline">Command output</span>
             </v-col>
-            <v-col>
-              <v-btn-toggle v-model="toggle_one" dense mandatory style="float: right;">
-                <v-btn small>Overview</v-btn>
-                <v-btn small :disabled="!commandData.payload">Table</v-btn>
-            </v-btn-toggle>
+            <v-col cols="4" class="text-right">
+              <v-chip label small :color="getColor(  commandData.status )">{{   commandData.status }}</v-chip>
             </v-col>
           </v-row>
     </v-card-title>
 
-    <v-card-text class="pr-1">
-      <v-simple-table v-if="toggle_one === 1 && commandData.payload" >
+    <v-card-text class="pa-1">
+      <v-row class="ml-1">
+        <v-col cols="10">
+          <p v-if="commandData.message">>{{ commandData.message }}</p>
+        </v-col>
+      </v-row>
+      <v-simple-table v-if="commandData.payload" ref="table" :height="`calc(100vh - ${tableDistanceTop}px)`">
         <thead>
           <tr>
             <th v-for="object in Object.entries(commandData.payload[0])" :key="object[0]">
@@ -32,24 +33,6 @@
           </tr>
         </tbody>
       </v-simple-table>
-
-      <v-simple-table v-if="toggle_one === 0">
-        <tbody>
-          <tr>
-            <th>Status</th>
-            <td><v-chip label small :color="getColor(  commandData.status )">{{   commandData.status }}</v-chip></td>
-          </tr>
-          <tr>
-            <th>Message</th>
-            <td v-if="  commandData.message">{{   commandData.message }}</td>
-            <td v-else>-</td>
-          </tr>
-          <tr v-if="  commandData.status === 'error'">
-            <th>Error type</th>
-            <td>{{   commandData.error_type }}</td>
-          </tr>
-        </tbody>
-      </v-simple-table>
     </v-card-text>
   </v-card>
 </template>
@@ -61,7 +44,7 @@ export default {
     return {
       name: "CommandOutput",
       toggle_one: 0,
-      tableHeight: null
+      tableDistanceTop: 200
     }
   },
   props: {
@@ -73,16 +56,11 @@ export default {
       if (status === "success") return "green";
       else return 'blue';
     },
-  },
-  watch: {
-    toggle_one(new_val, old_val) {
-    },
-    commandData() {
-      this.toggle_one = 0;
+    updateTableDistance() {
+      try {
+        this.tableDistanceTop = this.$refs.table.$el.getBoundingClientRect().top + 24;
+      } catch (error) {/** */}
     }
   },
-  updated() {
-    this.tableHeight = this.$store.getters.contentHeight-118+'px'
-  }
 }
 </script>
