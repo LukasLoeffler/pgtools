f8dce462ce5e5c3ef2807cd1ce7770d0b7840443
Lukas Löffler <lukas.loeffler@ibiz-innovation.de>
2021-10-04 13:01:11 +0200

    Decreased animation duration

--- client/src/views/Watcher.vue
+++ client/src/views/Watcher.vue
@@ -233,10 +233,16 @@ export default {
 </script>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 1s;
+.fade-enter-active {
+  transition: opacity 0.5s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+
+.fade-enter, .fade-leave-to {
   opacity: 0;
 }
 </style>
